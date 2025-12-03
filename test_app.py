b1b59abc16a823d2fc1488346a198f691e60ac83
Lithium <91140419+LithiumLi@users.noreply.github.com>
2025-12-03 15:52:19 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,15 +1,13 @@
 from app import addition
 
 def test_addition_positive():
-assert addition(2, 3) == 5
+    assert addition(2, 3) == 5
 
 def test_addition_negative():
-assert addition(-1, -2) == -3
-
-# Cas bonus
+    assert addition(-1, -2) == -3
 
 def test_addition_zero():
-assert addition(0, 0) == 0
+    assert addition(0, 0) == 0
 
 def test_addition_float():
-assert addition(2.5, 1.5) == 4.0
+    assert addition(2.5, 1.5) == 4.0
